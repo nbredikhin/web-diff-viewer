6f599decb069861ea5cfe857d40937811ee05a94
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2026-01-15 21:53:03 +0100

    Fix diff horizontal scroll backgrounds (#5)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'react-diff-view/style/index.css';
 import './styles.css';
 import './prism-theme.css';
-import 'react-diff-view/style/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
